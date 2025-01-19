495f8db86fc650be78716fa3d852e1abf3e67fd1
El32ol <100161241+El32ol@users.noreply.github.com>
2025-01-19 10:41:24 +0200

    cart

---
+++ app/Repositories/Cart/CartModelRepository.php
@@ -0,0 +1,87 @@
+<?php 
+
+namespace App\Repositories\Cart;
+
+use App\Models\Cart;
+use App\Models\Product;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Str;
+
+class CartModelRepository implements CartRepository
+{
+    protected $items;
+
+    public function __construct()
+    {
+        $this->items = collect([]);
+    }
+    public function get(): Collection
+    {
+        if(!$this->items->count())
+        {
+            $this->items = Cart::with('product')->get();
+        }
+        return $this->items;
+    }
+
+    public function add(Product $product, $quantity = 1)
+    {
+        
+        $item= Cart::where('product_id', $product->id)->first();
+        if(!$item)
+        {
+        $cart =  Cart::create([
+                'user_id' => Auth::id(),
+                'product_id' => $product->id,
+                'quantity' => $quantity
+            ]);
+            $this->get()->push($cart);
+            return $cart;
+        }
+
+        return $item->increment('quantity', $quantity);
+        
+    }
+
+    public function update($id, $quantity = 1)
+    {
+        Cart::where('id', $id)->first()
+                    ->update([
+                            'quantity' => $quantity,
+                        ]);
+
+    }
+
+    public function delete($id)
+    {
+         Cart::where('id', $id)->delete();
+    }
+
+    public function empty()
+    {
+         Cart::query()->delete();
+    }
+
+    public function total() : float
+    {
+        // return (float ) Cart::join('products' , 'products.id' , '=', 'carts.product_id')
+        //             ->selectRaw('SUM(products.price * carts.quantity) AS total')
+        //             ->value('total');
+        // return $this->get()->sum(function ($item) {
+        //     return $item->quantity * $item->product->price;
+        // });
+        return $this->get()->sum(function ($item) {
+            if ($item->product) {
+                return $item->quantity * $item->product->price;
+            }
+            return 0; // إذا لم يكن المنتج موجوداً
+        });
+        
+    }
+
+  
+
+
+}
